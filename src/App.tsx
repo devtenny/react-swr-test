4913792fb446692ba56d7ca1cb5262f3a0f99767
김태연 <ty980407@gmail.com>
2021-03-23 11:18:41 +0900

    ✨ App.tsx에 LocalProfile, Monitor 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import './App.css'
 import Counter from './components/Counter'
+import LocalProfile from './components/LocalProfile'
+import LocalProfileMonitor from './components/LocalProfileMonitor'
 import Profile from './components/Profile'
 
 function App() {
   return (
-    <>
+    <div style={{ padding: '50px' }}>
       <Profile id={35147} />
       <Counter />
-    </>
+      <LocalProfile />
+      <LocalProfileMonitor />
+    </div>
   )
 }
 
